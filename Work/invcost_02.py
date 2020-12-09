91af7e8dae11d1bd67f274dbca4c66285240924c
Mehmoodkha Pathan <mehmoodhkhap@vmware.com>
2020-12-09 22:13:22 +0530

    with sys argv

---
+++ Work/invcost_02.py
@@ -0,0 +1,15 @@
+#/usr/bin/python
+
+def inventory_cost(filename):
+	with open(filename,'rt') as FH:
+		headers = next(FH)
+		total = 0.0
+		for line in FH:
+			parts = line.split(',')
+			quant = int(parts[1])
+			price = float(parts[2])
+			total += quant*price
+	return total
+
+cost = inventory_cost('Data/inventory.csv')
+print('Total cost:', cost)
