b44c4259b35612c76bb065bc74259531f937d2d6
Mehmoodkha Pathan <mehmoodhkhap@vmware.com>
2020-12-09 21:50:16 +0530

    Modified with function

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,12 +1,15 @@
 #/usr/bin/python
 
-with open("Data/inventory.csv") as FH:
-	headers = next(FH)
-	total = 0.0
-	for line in FH:
-		parts = line.split(',')
-		quant = int(parts[1])
-		price = float(parts[2])
-		total += quant*price
+def inventory_cost(filename):
+	with open(filename) as FH:
+		headers = next(FH)
+		total = 0.0
+		for line in FH:
+			parts = line.split(',')
+			quant = int(parts[1])
+			price = float(parts[2])
+			total += quant*price
+	return total
 
- print("Total cost", total)
+cost = inventory_cost('Data/inventory.csv')
+print('Total cost:', cost)
