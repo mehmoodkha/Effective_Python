95e36c915533e046613c09cd0a1a51001e782831
Mehmoodkha Pathan <mehmoodhkhap@vmware.com>
2020-12-09 21:29:01 +0530

    Total cost calculation

---
+++ Work/invcost.py
@@ -0,0 +1,11 @@
+#/usr/bin/python
+
+with open("Data/inventory.csv") as FH:
+	headers = next(FH)
+	total = 0.0
+	for line in FH:
+		parts = line.split(',')
+		quant = int(parts[1])
+		price = float(parts[2])
+		total += quant*price
+ 	print(total)
